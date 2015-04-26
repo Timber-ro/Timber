1eba29a16c712ac0d43d9853cd5cc592ede333ce
Thomas Morris <thomas.morris@3pillarglobal.com>
2015-04-26 16:34:20 +0300

    Add some styling to the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def landing_page
+    @contractors = Contractor.all
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Timber
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.serve_static_files =     true  # make assets work in heroku
   end
 end
